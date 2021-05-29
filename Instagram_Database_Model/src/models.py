df569d279a4619af1d85c4baa0ce2a595c96b67b
eddiejosue14 <43359575+eddiejosue14@users.noreply.github.com>
2021-05-28 20:48:08 -0600

    DataBase Model IG
    DataBase Model IG

---
+++ Instagram_Database_Model/src/models.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(1000), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    url = Column(String(1000))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
